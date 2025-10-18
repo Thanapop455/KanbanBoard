a8f260b069627915ba87ba6366099760ca1807f9
thanapop455 <tanapop.somjai@gmail.com>
2025-10-19 04:45:47 +0700

    fix import path to api client (../../api/client)V2

--- Server/server.js
+++ Server/server.js
@@ -10,10 +10,10 @@ app.use(express.json({ limit: '20mb' }))
 // app.use(cors())
 
 app.use(cors({
-    origin:['https://kanbanboardv1.netlify.app', 'http://localhost:5173'],
+    origin: ['https://kanbanboardv1.netlify.app','http://localhost:5173'],
     credentials: true
-}))
-
+  }));
+  
 readdirSync('./routes')
     .map((c) => app.use('/api', require('./routes/' + c)))
     
