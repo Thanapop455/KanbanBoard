bdaf650ab009f753a02638bd6d8f4e5d8c7c0d40
thanapop455 <tanapop.somjai@gmail.com>
2025-10-19 05:46:08 +0700

    Mamameya

--- Client/src/pages/auth/Register.jsx
+++ Client/src/pages/auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import { toast } from "react-toastify";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
